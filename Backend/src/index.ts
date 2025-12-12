518f9b60510b21d2b3b994129561e8476605d216
rodrigoscorrea <rodrigosc616@gmail.com>
2025-12-12 22:21:25 +0000

    (fix) adapted for static serving monolith

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -24,6 +24,8 @@ validateEnv();
 const app = express();
 const PORT = process.env.PORT ?? 4466;
 
+app.set("trust proxy", 1);
+
 app.use(
   cors({
     credentials: true,
@@ -39,19 +41,34 @@ app.use(
     saveUninitialized: false,
     cookie: {
       secure: false,
+      httpOnly: true,
       maxAge: 2 * 60 * 60 * 1000,
+      sameSite: 'lax',
     },
   })
 );
 app.use(setLangCookie);
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "../client-build")));
+
+app.use((req, res, next) => {
+    console.log(`[REQUEST] ${req.method} ${req.url}`);
+    next();
+});
+
 app.use(router);
+
 
 // Log
 /*app.post('/v1/user', (req, res) => {
     console.log('Dados recebidos:', req.body);
     res.status(201).send('Usuário criado com sucesso');
 });*/
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client-build", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`server runing on ${PORT}`);
